41ebf72538e034220623ce54f68972eca5132a36
75776140 <madiyar03633@gmail.com>
2025-01-12 07:34:39 +0500

    update2 main.py

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import cv2 
 import pytesseract
 
+# The path to the tesseract executable
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 img = cv2.imread("images\image2.jpg")
 
+# Convert the image to grayscale for better thresholding 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Apply Otsu's thresholding and binary inversion to the grayscale image 
 ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
 
+# Define a rectangular structuring element for dilation
 rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
+# Apply dilation to the thresholding image to join text regions
 dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
 
+# Find contours from the dilated image
 contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+# Create a copy of the original image to draw rectangles on detected text
 im2 = img.copy()
 
+# Clear the content of the output file
 file = open("recognized.txt", "w+")
 file.write("")
 file.close
